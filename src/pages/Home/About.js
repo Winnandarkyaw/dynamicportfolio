5baa736c583b8aef4b22e846912e0ddf8d9fda01
Winnandarkyaw <winnandarkyaw2000@gmail.com>
2024-07-02 18:45:44 +0630

    firstday

---
+++ src/pages/Home/About.js
@@ -0,0 +1,59 @@
+import React from "react";
+import SectionTitle from "../../components/SectionTitle";
+
+function About() {
+  const skills = [
+    "React",
+    "Javascript",
+    "Node JS",
+    "Express JS",
+    "Mongo DB",
+    "Firebase",
+    "Python",
+    "Microsoft Power Platform",
+  ];
+  return (
+    <div>
+      <SectionTitle title="About Me" />
+      <div className="flex w-full items-center">
+        <div className="h-[70vh] w-1/2">
+          <lottie-player
+            src="https://assets6.lottiefiles.com/private_files/lf30_WdTEui.json"
+            background="transparent"
+            speed="1"
+            autoplay></lottie-player>
+        </div>
+        <div className="flex flex-col gap-5 w-1/2">
+          <p className="text-white">
+            Hello!I am Win Nandar Kyaw,a passionate developer in the field of
+            technology. I enjoy creating things that live on the internet. I
+            specialize in creating dynamic and impactful digital solutions.
+          </p>
+          <p className="text-white">
+            With a strong interest in both technology and business,aspiring to
+            be a Solution Architect.Currently,I am dedicated to mastering clean
+            code practices and design principles to elevate my technical
+            expertise.My goal is to integrate my coding skills and design
+            knowledge to provide innovative solutions that bridge the gap
+            between technical capabilities and business needs ensuring
+            efficiency and value in every project.
+          </p>
+        </div>
+      </div>
+      <div className="py-5">
+        <h1 className="text-tertiary text-2xl">
+          Here are a few technologies I've been working with recently:
+        </h1>
+        <div className="flex flex-wrap gap-10">
+          {skills.map((skill, index) => (
+            <div>
+              <h1 className="text-tertiary">{skill}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home/Intro.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Intro() {
+  return (
+    <div className="h-[80vh] bg-primary flex flex-col items-start justify-center gap-8 py-10">
+      <h1 className="text-white">Hi,I am</h1>
+      <h1 className="text-7xl text-secondary font-semibold">Win Nandar Kyaw</h1>
+      <h1 className="text-7xl text-white font-semibold">
+        I build things for web and app
+      </h1>
+      <p className="text-white w-2/3">
+        I am a Software Engineer.Currently I am working as a Software Developer
+        in Myanmar. Fast Learner and can share knowledge based on projects and
+        external through team members.
+      </p>
+      <button className="border-2 border-tertiary text-tertiary px-10 py-3 rounded">
+        Get Started
+      </button>
+    </div>
+  );
+}
+
+export default Intro;
